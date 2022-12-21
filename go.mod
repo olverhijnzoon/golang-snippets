78665c00590b9f188559d4463fc2f246616ee782
Olver Hijnzoon <olver.hijnzoon@gmail.com>
2022-12-21 14:41:13 +0100

    Fine tuning the store code files

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module golang-snippets
 go 1.19
 
 require (
-	github.com/olverhijnzoon/golang-snippets/mystore v0.0.0-20221220224131-3497947c7b97 // indirect
+	github.com/olverhijnzoon/golang-snippets/mystore v0.0.0-20221220224357-65ae9c86d596 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
 	golang.org/x/exp v0.0.0-20221215174704-0915cd710c24 // indirect
 	golang.org/x/tools v0.4.0 // indirect
--- store.go
+++ store.go
@@ -11,9 +11,9 @@ import (
 
 	In Go, it is generally considered good practice to declare an interface in the same package where it is used, rather than where it is implemented. This is because interfaces are a way of specifying the behavior of a type, rather than the implementation of that type. By declaring an interface in the same package where it is used, you make it clear to users of the interface what they can expect from types that implement it.
 
-	For example, suppose we have a package called store that defines an interface called Store for storing and retrieving data. If we declare the Store interface in the store package, users of the store package can see exactly what methods they can call on types that implement the Store interface.
+	For example, suppose we have a package called mystore that defines an interface called Store for storing and retrieving data. If we declare the Store interface in the mystore package, users of the mystore package can see exactly what methods they can call on types that implement the Store interface.
 
-	On the other hand, if we declare the Store interface in a separate package where it is implemented, users of the store package would not be able to see the methods of the Store interface unless they imported the implementation package. This could make it more difficult for users of the store package to understand how to use types that implement the Store interface.
+	On the other hand, if we declare the Store interface in a separate package where it is implemented, users of the mystore package would not be able to see the methods of the Store interface unless they imported the implementation package. This could make it more difficult for users of the mystore package to understand how to use types that implement the Store interface.
 
 	There are, however, cases where it may be appropriate to declare an interface in a separate package where it is implemented. For example, if the interface is intended to be a "well-known" interface that will be used by many packages, it may make sense to declare it in a separate package to make it easier for other packages to import and use.
 
