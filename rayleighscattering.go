60edf645a899c2b7f9a91e622d0b4d4db33131ed
Olver Hijnzoon <olver.hijnzoon@gmail.com>
2022-12-22 17:21:00 +0100

    Rayleigh scattering - improving on constant k with approximations

--- rayleighscattering.go
+++ rayleighscattering.go
@@ -17,24 +17,45 @@ func main() {
 
 		I = k * λ^-4
 
-		where:
+			where:
 
-		I is the intensity of the scattered light
-		k is a constant that depends on the size and composition of the particles
-		λ is the wavelength of the light
+			I .. is the intensity of the scattered light
+			k .. is a constant that depends on the size and composition of the particles
+			λ .. is the wavelength of the light
 		
 		This is code snippet that calculates the scattering for a given wavelength.
 	*/
 
-
-	// constant that depends on the size and composition of the particles
-	const k = 1.0 
-
 	// wavelength of the light, in nanometers
 	wavelength := 500.0
 
+		/*
+		The constant k depends on the size and composition of the particles that are scattering the light. It is generally difficult to calculate k accurately, as it requires knowledge of the exact size and composition of the particles. However, the following approximations that can be made:
+		
+		(1) If the particles are much smaller than the wavelength of the light, then k can be approximated as: k = (3 * π * n * d^2) / (2 * N)
+		(2) If the particles are much larger than the wavelength of the light, then k can be approximated as: k = (3 * π * n^2 * d^6) / (2 * N)
+			n .. is the refractive index of the particles
+			d .. is the diameter of the particles
+			N .. is the number of particles per unit volume
+		*/ 
+
+		// diameter of the particles, in nanometers
+		particleDiameter := 100.0 
+
+		// refractive index of the particles
+		refractiveIndex := 1.5
+		
+		// number of particles per unit volume
+		numberOfParticles := 1e9
+
+		// Calculate k using the approximation for small particles
+		k := (3 * math.Pi * refractiveIndex * math.Pow(particleDiameter, 2)) / (2 * numberOfParticles)
+
+		// Alternatively, calculate k using the approximation for large particles
+		//k := (3 * math.Pi * math.Pow(refractiveIndex, 2) * math.Pow(particleDiameter, 6)) / (2 * numberOfParticles)
+
 	// calculate the intensity of the scattered light
 	intensity := k * math.Pow(wavelength, -4)
-	
+
 	fmt.Println("Intensity of scattered light:", intensity)
 }
