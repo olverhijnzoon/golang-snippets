16ebb460d957a06cb61de4b678c10bce26f8572a
Olver Hijnzoon <olver.hijnzoon@gmail.com>
2023-01-08 13:14:37 +0100

    Performance test for quicksort

--- quicksort/quicksort_test.go
+++ quicksort/quicksort_test.go
@@ -1,9 +1,12 @@
 package quicksort
 
 import (
+	"fmt"
 	"math"
+	"math/rand"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestQuicksort(t *testing.T) {
@@ -29,6 +32,38 @@ func TestQuicksort(t *testing.T) {
 		result := Quicksortalgo(tc.input)
 		if !reflect.DeepEqual(result, tc.expected) {
 			t.Errorf("quicksort(%v) = %v, expected %v", tc.input, result, tc.expected)
+		}
+	}
+}
+
+func TestQuicksortPerformance(t *testing.T) {
+
+	/*
+		This test function sorts arrays with exponentially growing sizes, and measures the elapsed time for each array. It then prints the elapsed time for each array to the console, and checks whether it is above a certain threshold. If it is above the threshold, the test will fail. By testing the performance of quick sort on arrays with exponentially growing sizes, you can get a sense of how the time complexity of the algorithm scales with the size of the input. If the elapsed time grows roughly in proportion to n*log(n), as expected for quick sort, then this would be strong evidence that the algorithm has a time complexity of O(n*log(n)).
+	*/
+
+	// Test sorting arrays with exponential sizes
+	sizes := []int{100, 1000, 10000, 100000, 1000000, 10000000}
+	thresholds := []time.Duration{time.Millisecond * 10, time.Millisecond * 100, time.Millisecond * 1000, time.Second * 1, time.Second * 10, time.Second * 100}
+
+	for i, size := range sizes {
+		// Generate an array of random integers with the current size
+		arr := make([]int, size)
+		for j := range arr {
+			arr[j] = rand.Int()
+		}
+
+		// Measure the time it takes to sort the array
+		start := time.Now()
+		Quicksortalgo(arr)
+		elapsed := time.Since(start)
+
+		// Print the elapsed time to the console
+		fmt.Printf("Size %d: elapsed time = %s\n", size, elapsed)
+
+		// Check whether the elapsed time is above the threshold
+		if elapsed > thresholds[i] {
+			t.Errorf("quicksort took %s, which is above the threshold of %s", elapsed, thresholds[i])
 		}
 	}
 }
